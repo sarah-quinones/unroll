3eeb54acfc6268e543913afe387eb1f1e273e2c7
sarah <>
2022-03-12 14:00:53 +0100

    Implement unroll

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+#![feature(min_specialization)]
+
+struct Const<const N: usize>;
+
+trait UnrollImpl {
+    fn unroll<F: FnMut()>(f: F);
+}
+
+impl<const N: usize> UnrollImpl for Const<N> {
+    default fn unroll<F: FnMut()>(_: F) {
+        unimplemented!();
+    }
+}
+
+macro_rules! unroll_impl {
+    ($n:expr) => {
+        impl UnrollImpl for Const<$n> {
+            fn unroll<F: FnMut()>(mut f: F) {
+                seq_macro::seq!(N in 0..$n{ f(); });
+            }
+        }
+    };
+}
+
+impl UnrollImpl for Const<0> {
+    fn unroll<F: FnMut()>(_: F) {}
+}
+seq_macro::seq!(N in 1.. =256 {unroll_impl!(N);});
+
+pub fn unroll<const N: usize, F: FnMut()>(f: F) {
+    Const::<N>::unroll(f);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unroll() {
+        let mut a = 0;
+        unroll::<24, _>(|| a += 1);
+        assert_eq!(a, 24);
+    }
+}
